87304b1fb2314ba033d1463041f11bfc668427a7
weariness00 <ehddn917@gmail.com>
2022-10-19 02:45:06 +0900

    Animaiton 프레임워크 생성

---
+++ Project/Scripts/FrameWork/Animation.py
@@ -0,0 +1,24 @@
+from Scripts.Afx import *
+
+class Animation:
+    def __init__(self):
+        self.frame = 0
+        self.count = 0
+
+        self.countSpeed = 0
+
+        # image
+        self.image = None
+        self.image_type = None
+
+        pass
+
+    def __del__(self):
+        pass
+
+    def OnAnimation(self, frameTime):
+        self.image_type[0] = (int(self.count) % self.frame) * self.image_type[2]
+        self.count += frameTime * self.countSpeed
+        pass
+
+    pass
