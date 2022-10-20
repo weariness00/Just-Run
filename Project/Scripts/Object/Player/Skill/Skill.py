26ed4fd3ad7ef56077470ee43ce02f28a1a153d2
weariness00 <ehddn917@gmail.com>
2022-10-20 15:35:02 +0900

    Skill 만드는중

---
+++ Project/Scripts/Object/Player/Skill/Skill.py
@@ -0,0 +1,20 @@
+from Scripts.FrameWork.Object import *
+
+
+class Skill(Object):
+
+    def __init__(self):
+        super(Skill, self).__init__()
+        # 객체 초기화
+        self.collTime = 5
+        self.range = 0  # skill을 사용할 범위
+        # 스킬에 대한 능력치 관련
+        # 스킬에 쓸 떄 사용할 이미지 -> 이거는 skill들 만들떄 거기서 초기화
+        self.text = 'None'  # skill을 설명할 Text
+
+        # Timer
+        pass
+
+    def __del__(self):
+        pass
+    pass
