542162993f42e124dce79c3d6ffaaa637c969ed1
weariness00 <ehddn917@gmail.com>
2022-10-20 20:07:08 +0900

    Skill 추가

---
+++ Project/Scripts/Object/Player/Skill/StrongStrength.py
@@ -0,0 +1,16 @@
+# 강인한 체력
+
+from Scripts.Object.Player.Skill.Skill import *
+
+class StrongStrength(Skill):
+
+    def __init__(self):
+        super(StrongStrength, self).__init__()
+        # 객체 초기화
+        
+        pass
+
+    def __del__(self):
+        pass
+
+    pass
