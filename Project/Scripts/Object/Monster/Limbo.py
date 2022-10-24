7a540cd58bbef07290920356eadab248f4dc8b97
weariness00 <ehddn917@gmail.com>
2022-10-24 17:00:15 +0900

    스크립트 수정
    리펙토링

--- Project/Scripts/Object/Monster/Limbo.py
+++ Project/Scripts/Object/Monster/Limbo.py
@@ -19,22 +19,21 @@ class Limbo(Monster):
         self.collider.isTrigger = True
 
         # Animation
+        frame = 2
+        image_type =[0, 0, 305, 280]
         self.workingAni = Animation()
         self.workingAni.image = load_image('image/Monster/Limbo Monster/Working.png')
-        self.workingAni.image_type = [0, 0, 305, 280]
-        self.workingAni.frame = 2
+        self.workingAni.image_type = image_type
+        self.workingAni.frame = frame
         self.workingAni.countSpeed = 2.25
 
         self.deathAni = Animation()
         self.deathAni.image = load_image('image/Monster/Limbo Monster/Death.png')
-        self.deathAni.image_type = [0, 0, 305, 280]
-        self.deathAni.frame = 2
+        self.deathAni.image_type = image_type
+        self.deathAni.frame = frame
         self.deathAni.countSpeed = 4
 
         self.mainAnimation = self.workingAni
-
-        # Timer
-        self.deathStart = time.time()
         pass
 
     def __del__(self):
@@ -46,8 +45,8 @@ class Limbo(Monster):
             return
 
         super(Limbo, self).MoveMent()
-        self.CheckLifeTime()
-        self.OnAnimation()
+        super(Limbo, self).CheckLifeTime()
+        super(Limbo, self).OnAnimation()
         self.time.start = time.time()
         pass
 
@@ -55,9 +54,9 @@ class Limbo(Monster):
         if self.collider.isCollide is False:
             return
 
-        self.collider.OnCollider()
+        onColliderList = self.collider.OnCollider()
 
-        for collider in self.collider.onColliderList:
+        for collider in onColliderList:
             if collider.tag == "Player":
                 self.collider.isCollide = False
                 self.isMoveMent = False
@@ -66,38 +65,6 @@ class Limbo(Monster):
             pass
         pass
     pass
-
-    def OnAnimation(self):
-        self.mainAnimation.OnAnimation(self.time.OneFrameTime())
-
-        self.image = self.mainAnimation.image
-        self.image_type = self.mainAnimation.image_type
-
-        if self.transform.direction[0] > 0:
-            self.image_dir = 'h'
-        elif self.transform.direction[0] < 0:
-            self.image_dir = 'None'
-
-        if self.mainAnimation == self.deathAni and time.time() - self.deathStart > 1:
-            self.isActive = False
-            self.collider.isCollide = True
-            self.isMoveMent = True
-            self.mainAnimation = self.workingAni
-        pass
-
-    def CheckLifeTime(self):  # 생명주기 체크
-        if self.collider.isCollide is False:
-            return
-
-        lTime = time.time() - self.lifeStart
-        if lTime > self.lifeTime:
-            self.collider.isCollide = False
-            self.isMoveMent = False
-            self.mainAnimation = self.deathAni
-            self.deathStart = time.time()
-            pass
-        pass
-
     def Copy(self):
         return Limbo(self._targetPlayer)
 
--- Project/Scripts/Object/Monster/Monster.py
+++ Project/Scripts/Object/Monster/Monster.py
@@ -8,6 +8,7 @@ class Monster(Object):
         self._speed = None
         self._targetPlayer = target
         self.isMoveMent = True
+        self.isDeath = False
 
         # 생명주기
         self.lifeTime = 10
@@ -18,6 +19,15 @@ class Monster(Object):
         self.collider.InitTransform(self.transform)
         self.collider.tag = "Monster"
         self.collider.object = self
+
+        # Animaiton
+        self.mainAnimation = None
+        self.workingAni = None
+        self.attackAni = None
+        self.deathAni = None
+
+        # Timer
+        self.deathStart = time.time()
         pass
 
     def __del__(self):
@@ -31,3 +41,37 @@ class Monster(Object):
         realspeed = self._speed * self.time.OneFrameTime()
         self.transform.LooAtTarget(self._targetPlayer.transform, realspeed)
         pass
+
+    def OnAnimation(self):
+        self.mainAnimation.OnAnimation(self.time.OneFrameTime())
+
+        self.image = self.mainAnimation.image
+        self.image_type = self.mainAnimation.image_type
+
+        if self.transform.direction[0] > 0:
+            self.image_dir = 'None'
+        elif self.transform.direction[0] < 0:
+            self.image_dir = 'hd'
+
+        if self.isDeath is True and time.time() - self.deathStart > 1:
+            self.isActive = False
+            self.isDeath = False
+            self.collider.isCollide = True
+            self.isMoveMent = True
+            self.mainAnimation = self.workingAni
+        pass
+
+    def CheckLifeTime(self):  # 생명주기 체크
+        if self.collider.isCollide is False:
+            return
+
+        lTime = time.time() - self.lifeStart
+        if lTime > self.lifeTime:
+            self.collider.isCollide = False
+            self.isMoveMent = False
+            self.isDeath = True
+            self.mainAnimation = self.deathAni
+            self.deathStart = time.time()
+            pass
+        pass
+    pass
