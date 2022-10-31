c53b23296c6393052d0bfe88110176c00acecb50
weariness00 <ehddn917@gmail.com>
2022-11-01 00:16:29 +0900

    Type 추가
    skill의 Type을 결정짓는 멤버 변수 추가

--- Project/Scripts/Object/Player/Skill/Skill.py
+++ Project/Scripts/Object/Player/Skill/Skill.py
@@ -9,6 +9,7 @@ class Skill(Object):
         self.collTime = 5
         self.range = 0  # skill을 사용할 범위
         self.level = 0
+        self.skill_Type = None    # Active, Passive
 
         # 스킬에 대한 능력치 관련
         # 스킬에 쓸 떄 사용할 이미지 -> 이거는 skill들 만들떄 거기서 초기화
