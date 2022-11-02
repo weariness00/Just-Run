63907489f3d76196492e8d1d7d7dc1ae544a3e0e
weariness00 <ehddn917@gmail.com>
2022-11-03 02:12:43 +0900

    Revert "Revert "Player Dash 상태""
    This reverts commit 47bce6e78642b6485c6e28f60c38202bee3bdfeb.

---
+++ Project/Scripts/Object/Player/Animation/Player_Dash.py
@@ -0,0 +1,36 @@
+from Scripts.Object.Player.Animation.Player_Static_State import *
+from Scripts.Object.Player.Animation.Player_Working import Player_Working
+
+
+class Player_Dash:
+
+    @staticmethod
+    def enter(self, event):
+        Static_State.enter(self, event)
+        self.mainAnimation = self.dashAni
+        self.speed += 300
+        pass
+
+    @staticmethod
+    def exit(self):
+        self.speed -= 300
+        pass
+
+    @staticmethod
+    def do(self):
+        dTime = time.time() - self.skill.dashTime
+        if dTime > 0.5:
+            self.skill.isSkillOn = False
+
+            self.cur_state.exit(self)  # 현재 이벤트 탈출
+            self.cur_state = Player_Working  # 다음 이벤트 저장
+            self.cur_state.enter(self, null)  # 다음 이벤트 호출
+            pass
+        Static_State.do(self)
+        pass
+
+    @staticmethod
+    def draw(self):
+        pass
+
+    pass
