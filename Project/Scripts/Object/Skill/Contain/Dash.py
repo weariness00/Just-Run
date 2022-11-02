af1804094f69ed770dc139d40e2fc52051c46a51
weariness00 <ehddn917@gmail.com>
2022-11-03 02:14:22 +0900

    상태 리펙토링에 따른 수정
    Dash 상태에서 사용가능한 멤버 변수 제외
    함수 내 알고리즘도 제외

--- Project/Scripts/Object/Skill/Contain/Dash.py
+++ Project/Scripts/Object/Skill/Contain/Dash.py
@@ -1,11 +1,15 @@
 # 돌진
 
 from Scripts.Object.Skill.Skill import *
+from Scripts.Object.Player.Player import Player
+from Scripts.Object.Player.Animation.PlayerAnimationController import *
 
 class Dash(Skill):
     def __init__(self):
         super(Dash, self).__init__()
         # 객체 초기화
+        self.dashTime = 0
+
         self.image = load_image('image/UI/Skill/Dash.png')
         self.image_type = [0, 0, 32, 32]
         self.skill_Type = 'Active'
@@ -20,12 +24,24 @@ class Dash(Skill):
         pass
 
     def OnSkill(self):
+        self.isSkillOn = True
+        self.dashTime = time.time()
+        Player.this.isGot = True
+        Player.this.collider.isCollide = False
+        Player.this.gotTime = time.time()
+        Player.this.gotDurationTime = 0.5
         pass
 
     def Handle_Event(self, event):
-        if (event.type, event.key) == (SDLK_DOWN, SDLK_LSHIFT) or \
-                (event.type, event.key) == (SDLK_DOWN, SDLK_RSHIFT):
+        if self.isSkillOn is True:
+            return
+
+        if (event.type, event.key) == (SDL_KEYDOWN, SDLK_LSHIFT) or \
+                (event.type, event.key) == (SDL_KEYDOWN, SDLK_RSHIFT):
             self.OnSkill()
+            Player.this.cur_state.exit(Player.this)  # 현재 이벤트 탈출
+            Player.this.cur_state = next_state[Player_Dash][null]  # 다음 이벤트 저장
+            Player.this.cur_state.enter(Player.this, event)  # 다음 이벤트 호출
         pass
 
     pass
