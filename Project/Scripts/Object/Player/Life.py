f0e306ba1597151c34caa87166a4d379eaed5f11
weariness00 <ehddn917@gmail.com>
2022-11-03 16:21:41 +0900

    프레임 관리
    image 변환시 프레임이 그대로 적용되도록함

--- Project/Scripts/Object/Player/Life.py
+++ Project/Scripts/Object/Player/Life.py
@@ -2,21 +2,25 @@ from Scripts.FrameWork.Object import *
 from Scripts.FrameWork.Animation import *
 
 class Life(Object):
-    
+    renderList = None  # 동적할당해주는 것들은 자체적으로 렌더를 가짐
+    updateList = None
     def __init__(self, position):
         super(Life, self).__init__()
         self.name = 'Life'
+
 
         # Transform
         self.transform.Position += position
         self.transform.Scale *= 5
 
+        count = random.randint(0, 4)
         # Animation
         self.redFireAni = Animation()
         self.redFireAni.image = load_image('image/UI/Life/Fire.png')
         self.redFireAni.image_type = [0, 0, 15, 20]
         self.redFireAni.frame = 4
         self.redFireAni.countSpeed = 5
+        self.redFireAni.count = count
 
         self.blueFireAni = Animation()
         self.blueFireAni.image = load_image('image/UI/Life/BlueFire.png')
