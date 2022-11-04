1a9904d8d5fe99e43c617ac8e15b565a89ac5b5b
weariness00 <ehddn917@gmail.com>
2022-11-04 15:17:22 +0900

    GameOver 이미지 추가
    GameOver시에 플레이어의 이미지 변환 추가

--- Project/Scripts/State/GameOver_State.py
+++ Project/Scripts/State/GameOver_State.py
@@ -1,16 +1,45 @@
 from Scripts.Afx import *
+from Scripts.FrameWork.Object import Object
+from Scripts.FrameWork.Animation import Animation
+from Scripts.FrameWork.Render import Render
+
 import Scripts.FrameWork.game_framework as game_framework
 import Scripts.State.GamePlay_State as Game_State
 import Scripts.State.Lobby_State as Lobby_State
 
+player = None
+updateList = None
+renderList = None
 
 def enter():
+    global player
+    global updateList, renderList
 
+    player = Object()
+    player.transform.Position += [Instance.windowSize[0]//2, Instance.windowSize[1]//2]
+    player.transform.Scale *= 2
+    player.ani = Animation()
+    player.ani.image = load_image('image/GameOver/Player_GameOver.png')
+    player.ani.image_type = [0,0,40,60]
+    player.ani.frame = 12
+    player.ani.countSpeed = 10
+    player.image = player.ani.image
+    player.image_type = player.ani.image_type
+
+    render = Render()
+    render.AddRenderObject(player)
+
+    updateList = []
+    renderList = []
+
+    updateList.append(player)
+    renderList += [render]
     pass
 
 # finalization code
 def exit():
-
+    global player
+    del player
     pass
 
 def handle_events():
@@ -29,13 +58,19 @@ def handle_events():
 
 
 def update():
+    global updateList
 
+    player.ani.OnAnimation(player.time.OneFrameTime())
+    player.time.start = time.time()
+    for obj in updateList:
+        obj.Update()
     pass
 
 def draw():
-    clear_canvas()
+    global renderList
 
-    update_canvas()
+    for obj in renderList:
+        obj.UIDraw()
     pass
 
 def pause():
