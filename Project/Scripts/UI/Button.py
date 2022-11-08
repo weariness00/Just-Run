2701a09f22251c2ba5f76d90f6dd6e9df1a9e33d
weariness00 <ehddn917@gmail.com>
2022-11-09 02:56:25 +0900

    Button 스크립트 추가
    마우스를 버튼에 올렸을떄 OnMouse
    마우스를 버튼에 올리고 클릭했을떄 ClickMouse

---
+++ Project/Scripts/UI/Button.py
@@ -0,0 +1,62 @@
+from Scripts.FrameWork.Object import *
+
+class Button(Object):
+    image = load_image('image/UI/Button.png')
+    def __init__(self):
+        super(Button, self).__init__()
+        self.isClick = False
+
+        self.image = Button.image
+        self.image_type = [0, 0, 167, 48]
+        self.frameCount = 0
+        pass
+
+    def __del__(self):
+        super(Button, self).__del__()
+        pass
+
+    def Update(self):
+        self.Handle_Event()
+
+        self.image_type[0] = self.image_type[2] * self.frameCount
+        pass
+
+    def Handle_Event(self):
+        w = self.image_type[2] * self.transform.Scale[0] / 2
+        h = self.image_type[3] * self.transform.Scale[1] / 2
+        dot = [[self.transform.Position[0] - w, self.transform.Position[1] - h],
+               [self.transform.Position[0] + w, self.transform.Position[1] + h]]
+        for event in Object.events:
+            self.OnMouse(event, dot)
+            self.ClickMouse(event, dot)
+            pass
+        pass
+
+    def OnMouse(self, event, dot):
+        if event.type == SDL_MOUSEMOTION:
+            x = event.x
+            y = Instance.windowSize[1] - event.y
+            if dot[0][0] <= x <= dot[1][0] and dot[0][1] <= y <= dot[1][1] and self.frameCount != 2:
+                self.frameCount = 1
+            elif self.frameCount != 2:
+                self.frameCount = 0
+        pass
+    pass
+
+    def ClickMouse(self, event, dot):
+        if event.type == SDL_MOUSEBUTTONDOWN or event.type == SDL_MOUSEBUTTONUP:
+            x = event.x
+            y = Instance.windowSize[1] - event.y
+
+        if event.type == SDL_MOUSEBUTTONDOWN:
+            if event.button == SDL_BUTTON_LEFT and \
+                    dot[0][0] <= x <= dot[1][0] and dot[0][1] <= y <= dot[1][1]:
+                self.frameCount = 2
+        elif event.type == SDL_MOUSEBUTTONUP:
+            if event.button == SDL_BUTTON_LEFT and \
+                    dot[0][0] <= x <= dot[1][0] and dot[0][1] <= y <= dot[1][1]:
+                self.isClick = True
+            else:
+                self.frameCount = 0
+        pass
+    pass
