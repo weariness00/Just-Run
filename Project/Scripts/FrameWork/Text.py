0048a20b038b10bded219a9edbbf0e8927baec5a
weariness00 <ehddn917@gmail.com>
2022-11-09 02:57:58 +0900

    Text 수정
    사이즈를 재조정 할 수 있는 함수를 추가

--- Project/Scripts/FrameWork/Text.py
+++ Project/Scripts/FrameWork/Text.py
@@ -9,10 +9,19 @@ class Text:
         self.font_Name = load_font('Font/Name.ttf', size)
         self.font_Explain = load_font('Font/Explain.ttf', size)
         self.font = self.font_Name
-        self.color = (0, 0, 0)
+        self.color = [255, 255, 255]
         pass
 
     def __del__(self):
+        pass
+
+    def ReSize(self, size):
+        del self.font_Name, self.font_Explain
+
+        self.font_Name = load_font('Font/Name.ttf', size)
+        self.font_Explain = load_font('Font/Explain.ttf', size)
+
+        self.font = self.font_Name
         pass
 
     def Copy(self):
