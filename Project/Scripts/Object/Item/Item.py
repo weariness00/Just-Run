74d816bc1a1b05a4f5df6311e153d9d1285051e3
weariness00 <ehddn917@gmail.com>
2022-11-17 16:46:02 +0900

    Item 제작
    모든 Item들이 상속받을 클래스

--- Project/Scripts/Object/Item/Item.py
+++ Project/Scripts/Object/Item/Item.py
@@ -1,12 +1,21 @@
 from Scripts.FrameWork.Object import *
-
+from Scripts.Object.Player.Player import Player
 
 class Item(Object):
     renderList = None
     def __init__(self):
         super(Item, self).__init__()
+        self.isMoveMent = False
 
         self.name = 'Item'
+        self.isActive = False
+
+        # Collide
+        self.collider = Collide()
+        self.collider.tag = "Item"
+        self.collider.object = self
+        self.collider.InitTransform(self.transform)
+        self.collider.SetCollideBox(numpy.array([[0, 0], [32, 32]]))
 
         Item.renderList.AddObject(self)
         pass
@@ -15,8 +24,27 @@ class Item(Object):
         super(Item, self).__del__()
         pass
 
-    def OnItem(self, position):
-        self.isActive = True
-        self.transform.Position = numpy.copy(position)
+    def Enable(self):
+        self.collider.isCollide = True
+        self.isMoveMent = False
+        pass
 
+    def Update(self):
+        self.MoveMent()
+        pass
+
+    def MoveMent(self):
+        if self.isMoveMent is False:
+            return
+
+        self.transform.LooAtTarget(Player.this.transform, 500 * FrameTime.fTime)
+
+        dis = Instance.Distance(Player.this.transform.Position, self.transform.Position)
+        if  dis <= 10:
+            self.SetActive(False)
+        pass
+
+    def OnItem(self, position):
+        self.SetActive(True)
+        self.transform.Position = numpy.copy(position)
     pass
