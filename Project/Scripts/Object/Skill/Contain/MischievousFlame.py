8848b139d4adbdd107b1211a22e43a071f936b40
weariness00 <ehddn917@gmail.com>
2022-11-19 17:01:44 +0900

    제멋대로 불꽃 Skill 추가
    카메라에서 제멋대로 돌아다니며 몬스터를 겁주는 스킬
    몬스터에게 닿으면 몬스터를 느리게함

--- Project/Scripts/Object/Skill/Contain/MischievousFlame.py
+++ Project/Scripts/Object/Skill/Contain/MischievousFlame.py
@@ -1,35 +1,141 @@
 # 장난꾸러기 불꽃
-
 from Scripts.UI.Text import Text
 from Scripts.Object.Player.Player import *
+from Scripts.FrameWork.LayCast import CircleLay
 
-class WildFlame(Object):
+class FlameFlower(Object):
+    idleImage = load_image('image/FlameFlower/Idle.png')
+    workImage = load_image('image/FlameFlower/Work.png')
     def __init__(self):
-        super(WildFlame, self).__init__()
+        super(FlameFlower, self).__init__()
         self.isActive = False
+        self.isMoveMent = True
+
+        self.lifeTime = 5
+        self.startTime = 0
+        self.endTime = 0
+
+        self.randomPosition = 0
+        self.moveTime = 0
+        self.speed = 0
+
+        self.transform.Scale *= 0.25
+
+        # LayCast
+        self.circleLay = CircleLay(self.transform, 100)
+        self.circleLay.name = "FlameFlower CircleLay"
+        self.circleLay.SetActive(False)
+        self.circleLay.cameraType = 'UI'
+
+        # Animation
+        self.idleAni = Animation()
+        self.idleAni.image = FlameFlower.idleImage
+        self.idleAni.image_type = [0,0,244,525]
+        self.idleAni.frame = 4
+        self.idleAni.countSpeed = 5
+
+        self.workAni = Animation()
+        self.workAni.image = FlameFlower.workImage
+        self.workAni.image_type = [0,0,350,525]
+        self.workAni.frame = 3
+        self.workAni.countSpeed = 10
+
+        self.image = self.workAni.image
+
+        Object.renderList.AddObject(self, 1)
         pass
 
     def __del__(self):
-        super(WildFlame, self).__del__()
+        super(FlameFlower, self).__del__()
         pass
 
     def Enable(self):
-        self.transform.
+        self.circleLay.SetActive(True)
+        self.isMoveMent = True
+
+        halfWindowSize = Instance.windowSize//2
+        self.transform.Position = numpy.copy(Player.this.transform.Position)
+        self.transform.Position[0] += random.randint(-halfWindowSize[0], halfWindowSize[0])
+        self.transform.Position[1] += random.randint(-halfWindowSize[1], halfWindowSize[1])
+        self._SetRandomPosition()
+        self.startTime = time.time()
         pass
 
-class Clone_Techniqu(Skill):
+    def Update(self):
+        if time.time() - self.endTime > 5:
+            self.isMoveMent = True
+            self.isDraw = True
+            self.startTime = time.time()
 
+        self.MoveMent()
+        pass
+
+    def MoveMent(self):
+        if not self.isMoveMent:
+            return
+
+        if time.time() - self.startTime > self.lifeTime:
+            self.isMoveMent = False
+            self.isDraw = False
+            self.endTime = time.time()
+            return
+
+        self.workAni.OnAnimation(FrameTime.fTime)
+        self.image_type = self.workAni.image_type
+
+        if abs(Player.this.transform.Position[0] - self.transform.Position[0]) > Instance.windowSize[0]//2:
+            self._SetRandomPosition()
+            self.speed = Player.this.speed + 300
+        elif abs(Player.this.transform.Position[1] - self.transform.Position[1]) > Instance.windowSize[1]//2:
+            self._SetRandomPosition()
+            self.speed = Player.this.speed + 300
+
+        self.transform.LooAtTarget(self.randomPosition, self.speed * FrameTime.fTime)
+
+        if self.transform.direction[0] > 0:
+            self.image_dir = 'h'
+        elif self.transform.direction[0] < 0:
+            self.image_dir = 'None'
+
+        if time.time() - self.moveTime > 1:
+            self._SetRandomPosition()
+        pass
+
+    def _SetRandomPosition(self):
+        halfWindowSize = Instance.windowSize // 2
+        self.randomPosition = numpy.copy(Player.this.transform.Position)+\
+                              [random.randint(-halfWindowSize[0], halfWindowSize[0]),
+                               random.randint(-halfWindowSize[1], halfWindowSize[1])]
+        self.speed = random.randint(100, 500)
+        self.moveTime = time.time()
+        pass
+
+    def OnCollide(self):
+        if not self.circleLay.isActive:
+            return
+
+        # TODO Lay 충돌 미구현
+        # UI 상태인 Lay의 충돌을 미구현
+        colliderList = self.circleLay.OnLayCast()
+        for collider in colliderList:
+            if collider.tag == "Monster":
+                collider.object.addSpeed = -collider.object.speed//2
+        pass
+
+    pass
+
+class MischievousFlame(Skill):
     def __init__(self):
-        super(Clone_Techniqu, self).__init__()
+        super(MischievousFlame, self).__init__()
         # 객체 초기화
-        self.skill_Type = 'Active'
+        self.skill_Type = 'Passive'
         self.coolTime = 10
 
-        self.image = load_image('image/UI/Skill/Clone_Techniqu.png')
-        self.image_type = [0, 0, 64, 64]
+        self.image = load_image('image/UI/Skill/MischievousFlame.png')
+        self.image_type = [0, 0, 244, 525]
 
         # Transform
-        self.transform.Scale *= 0.5
+        self.transform.Scale *= 0.1
 
         # Text 초기화
         self.skillName = '[장난꾸러기 불꽃]'
@@ -39,31 +145,25 @@ class Clone_Techniqu(Skill):
         self.explain[1].text = '불꽃에 닿는 몬스터는 이동속도가 50%만큼 느려집니다.'
         self.explain.append(Text())
         self.explain[2].font = self.explain[2].fontList['Explain']
-        self.explain[2].text = '사라진 후 5초뒤 나타납니다. 최대 Level 3'
+        self.explain[2].text = '사라진 후 5초뒤 나타납니다. 최대 Level 5'
 
         # 능력 초기화
-        self.flame = WildFlame()
-
+        self.flame = FlameFlower()
         pass
 
     def __del__(self):
-        super(Clone_Techniqu, self).__del__()
-        pass
-
-    def Update(self):
-        if time.time() - self.onSkillTime >= self.coolTime:
-            self.onSkillTime = time.time()
-
+        super(MischievousFlame, self).__del__()
         pass
 
     def OnSkill(self):
-        super(Clone_Techniqu, self).OnSkill()
+        super(MischievousFlame, self).OnSkill()
+        self.flame.lifeTime = self.level * 5
         self.flame.SetActive(True)
         pass
 
     def LevelUp(self):
-        super(Clone_Techniqu, self).LevelUp()
-        if self.level >= 2:
+        super(MischievousFlame, self).LevelUp()
+        if self.level >= 5:
             self.isMaxLevel = True
         pass
 
