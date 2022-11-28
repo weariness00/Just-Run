4009365a1b4c5c17a26457b6f95142f7f17681e7
weariness00 <ehddn917@gmail.com>
2022-11-28 21:48:30 +0900

    난이도 추가
    Easy, Normal, Hard
    MonsterPool의 data를 leveling으로 옮김

--- Project/Data/LevelingData.py
+++ Project/Data/LevelingData.py
@@ -1,25 +1,40 @@
 from openpyxl import load_workbook
+from Scripts.Object.Monster.MonsterPool import MonsterPool
 
 class LevelingData:
     def __init__(self, level):
         self.file = load_workbook('Data/LevelingData.xlsx', data_only = True)
         self.level = self.file[level]
-
+        self.maxLevel = self.level["K2"].value
         pass
 
     def __del__(self):
         pass
 
-    def SetData(self, pools, level):
-        maxLevel = self.level["F2"].value
-        if maxLevel > level:
-            level = maxLevel
+    def SetTimeData(self, playTimer):
+        playTimer.winTime = self.level["L2"].value
+        playTimer.levelUpLengthTime = self.level["M2"].value
+        pass
+    def SetDefaultPoolData(self, pools):
+        maxPool = self.level["N2"].value
+        for i in range(maxPool):
+            type = self.level.cell(row=i + 2, column=1).value
+            maxCount = self.level.cell(row=i + 2, column=2).value
+            sapwnCount = self.level.cell(row=i + 2, column=3).value
+            coolTime = self.level.cell(row=i + 2, column=4).value
+            pools.append(MonsterPool(type, maxCount, sapwnCount, coolTime))
+            pass
+        pass
+
+    def SetPoolData(self, pools, level):
+        if level > self.maxLevel:
+            level = self.maxLevel
 
         r = 22
         c = level * 3
         for pool in pools:
             pool.spawnCount += self.level.cell(row=r, column=c).value
-            pool.coolTime += self.level.cell(row=r, column=c).value
+            pool.coolTime += self.level.cell(row=r, column=c + 1).value
             r += 1
             pass
         pass
